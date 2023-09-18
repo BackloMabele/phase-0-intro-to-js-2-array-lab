d558eebcfa056cb515b20a6951387bf08ab3bf78
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-18 21:58:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution here!
+
+    let cats = ["Milo", "Otis", "Garfield"]
+    Cats.push("Ralph")
+    cats.unshift("Bob")
+    cats.pop()
+    cats.shift()
+    let appendCat = [...cats, "Broom"]
+    let prependCat = ["Arnold", ...cats]
+    cats.slice(-1)
+    cats.slice(1)
